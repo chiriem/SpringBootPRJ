2d0d53121a9abe9e012fa46e857a4262f470dd68
chiriem <maoru23@gmail.com>
2022-03-24 10:45:40 +0900

    goodddd

---
+++ src/main/java/kopo/poly/persistance/mongodb/IMelonMapper.java
@@ -0,0 +1,15 @@
+package kopo.poly.persistance.mongodb;
+
+import kopo.poly.dto.MelonDTO;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IMelonMapper {
+
+    int insertSong(List<MelonDTO> pList, String colNm) throws Exception;
+
+    List<MelonDTO> getSongList(String colNm) throws Exception;
+
+    List<Map<String, Object>> getSingerSongCnt(String colNm) throws  Exception;
+}
