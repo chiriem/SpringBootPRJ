a91aa54c478436d6148adb40a77ec681f2bc92e7
chiriem <maoru23@gmail.com>
2022-04-11 08:58:31 +0900

    조회수 tn

--- src/main/java/kopo/poly/service/impl/NoticeService.java
+++ src/main/java/kopo/poly/service/impl/NoticeService.java
@@ -10,7 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-
 @Slf4j
 @Service("NoticeService")
 public class NoticeService implements INoticeService {
